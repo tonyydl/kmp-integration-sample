71a4023b8c2658b73fb78d0c3f87cc0455748f16
pahill <pamela.a.hill@gmail.com>
2025-06-29 17:07:10 +0200

    Update dependencies + wizard to June 2025's versions.

--- app/src/main/java/com/jetbrains/simplelogin/androidapp/ui/login/LoginActivity.kt
+++ app/src/main/java/com/jetbrains/simplelogin/androidapp/ui/login/LoginActivity.kt
@@ -4,9 +4,11 @@ import android.app.Activity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.jetbrains.simplelogin.androidapp.R
 import com.jetbrains.simplelogin.androidapp.data.LoginDataSource
 import com.jetbrains.simplelogin.androidapp.data.LoginDataValidator
@@ -15,18 +17,23 @@ import com.jetbrains.simplelogin.androidapp.data.LoginRepository
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
 
         setContent {
             MaterialTheme {
+                val loginViewModel = viewModel {
+                    LoginViewModel(
+                        loginRepository = LoginRepository(
+                            dataSource = LoginDataSource()
+                        ),
+                        dataValidator = LoginDataValidator()
+                    )
+                }
+
                 Surface() {
                     LoginScreen(
-                        viewModel = LoginViewModel(
-                            loginRepository = LoginRepository(
-                                dataSource = LoginDataSource()
-                            ),
-                            dataValidator = LoginDataValidator()
-                        ),
+                        viewModel = loginViewModel,
                         onLoginSuccess = {
                             // Show welcome message
                             val successResult = it.success
